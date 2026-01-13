017193e69af0a6eeaef6937bf886fe15194875fc
Gopal prashad <gopubaghel90@gmail.com>
2026-01-13 14:42:19 +0530

    Added reverse array problem

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,34 @@
+// ======================================
+// Problem: Reverse an Array
+// Data Structure: Array
+// Approach: Two Pointer
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+// ======================================
+
+class ReverseArray {
+
+    public static void reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        reverse(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
